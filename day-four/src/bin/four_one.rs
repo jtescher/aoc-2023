d6c7b2b3504c2e75c249bcb23602931831401c34
Julian Tescher <jatescher@gmail.com>
2023-12-04 17:35:15 -0500

    Add day 4

---
+++ day-four/src/bin/four_one.rs
@@ -0,0 +1,31 @@
+use std::collections::HashSet;
+
+fn main() {
+    let input = include_str!("input.txt");
+
+    let solution: u32 = input
+        .lines()
+        .filter_map(|line| {
+            let (correct, guesses) = line.split_once(':').unwrap().1.split_once('|').unwrap();
+            let correct = correct
+                .trim()
+                .split_whitespace()
+                .map(|num| num.parse().unwrap())
+                .collect::<HashSet<u32>>();
+            let count = guesses
+                .trim()
+                .split_whitespace()
+                .map(|num| num.parse().unwrap())
+                .filter(|num| correct.contains(num))
+                .count();
+
+            if count > 0 {
+                Some(2u32.pow((count - 1) as u32))
+            } else {
+                None
+            }
+        })
+        .sum();
+
+    println!("{solution}")
+}
---
+++ day-four/src/bin/four_two.rs
@@ -0,0 +1,37 @@
+use std::collections::{HashMap, HashSet};
+
+fn main() {
+    let input = include_str!("input.txt");
+
+    let solution = input
+        .lines()
+        .rev()
+        .fold(HashMap::new(), |mut acc, line| {
+            let (card_num, card_data) = line.trim_start_matches("Card ").split_once(':').unwrap();
+            let card_num = card_num.trim().parse::<usize>().unwrap();
+            let (correct, guesses) = card_data.split_once('|').unwrap();
+
+            let correct = correct
+                .trim()
+                .split_whitespace()
+                .map(|num| num.parse().unwrap())
+                .collect::<HashSet<usize>>();
+            let hits = guesses
+                .trim()
+                .split_whitespace()
+                .map(|num| num.parse().unwrap())
+                .filter(|num| correct.contains(num))
+                .count();
+            let total = (1..=hits)
+                .map(|num| *acc.get(&(card_num + num)).unwrap())
+                .sum::<usize>()
+                + 1;
+
+            acc.insert(card_num, total);
+            acc
+        })
+        .values()
+        .sum::<usize>();
+
+    println!("{solution}")
+}
